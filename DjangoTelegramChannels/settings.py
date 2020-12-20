c962cc30197c47753b7af66693c88c35f3a1e026
Vasilkov-E <61387315+Vasilkov-E@users.noreply.github.com>
2020-12-20 16:04:31 +0300

    Update settings.py

--- DjangoTelegramChannels/settings.py
+++ DjangoTelegramChannels/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_t-mrtt^x#3#ppk+-b)i$d26mzdm_2o5=8nbd$93i(yh(wt#q4'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
